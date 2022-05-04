71681e05a16c36bd430d96bf76dd509e6dd397db
timothy hoover <bnicholehilliard@gmail.com>
2022-05-04 11:53:54 -0700

    cypress

--- cypress/integration/order.spec.js
+++ cypress/integration/order.spec.js
@@ -1,3 +1,15 @@
 it('sanity check', () =>{
     expect(10+20).to.equal(30)
 })
+
+describe('visit bloom eats website', ()=>{
+    beforeEach(()=> {
+        cy.visit('http://localhost:3000')
+    })
+    it('visiting the website works', ()=>{
+        cy.contains('Bloom Eats')
+    })
+    it('can order a pizza', ()=> {
+        cy.get(`[pizza-test-id="root"]`)
+    })
+})
